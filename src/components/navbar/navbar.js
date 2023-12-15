408ae5a8f13755501d07ad4088deddf31b07ea96
Jonathan Torres <jontorres@MacBook-Pro.local>
2023-12-14 18:08:40 -0800

    saving progress

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/JT.png";
 import "./navbar.scss";
 
 export default function NavBar() {
   return (
     <section className="navbar">
-      <div className="logo">logo</div>
+      <div className="div-logo">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
       <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/experience">Experience</Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/about" className="link">
+          About
+        </Link>
+        <Link to="/experience" className="link">
+          Experience
+        </Link>
       </div>
     </section>
   );
