416a012205d3a39b5c16ae2c3fe98c95c8da11e6
Jonathan Torres <jontorres@MacBook-Pro.local>
2024-01-12 11:23:55 -0800

    saving progress

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ This is the source code for my Portfolio Project
 - Reactjs
 - Zustand
 - Typed.js
+
+## Updates
+
+- Updates reguarding current and future updates can be found in the footer link "Site Updates"
--- src/scenes/updates/updates.js
+++ src/scenes/updates/updates.js
@@ -16,10 +16,27 @@ export default function Updates({ pageName }) {
       <div className="updates-container">
         <ul className="updates-list">
           <li>12/20/2023: Added Tech Stack and updates pages</li>
+          <li>1/4/2024: Handled Navbar and footer mobile view</li>
         </ul>
         <h3 className="title">Upcoming...</h3>
         <ul className="updates-list">
           <li>TBD: Hanlde mobile view</li>
+          <li>TBD: Update tech stack</li>
+          <li>TBD: Refactor current HTML elements</li>
+          <li>TBD: Refactor current CSS files</li>
+          <li>
+            TBD: Migrate Updates content to JSON and create resuable components
+          </li>
+          <li>TBD: Standardize card components</li>
+          <li>TBD: Handle overflow of contents on smaller viewports</li>
+        </ul>
+
+        <h3 className="title">Current Known Bugs</h3>
+        <ul className="updates-list">
+          <li>
+            - After visiting all pages, home screen intro no longer centered due
+            to new component
+          </li>
         </ul>
       </div>
     </section>
